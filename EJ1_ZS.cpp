0b047d84c02800ccc69b9e6ac4076356448ac97e
dulce0308 <166740721+dulce0308@users.noreply.github.com>
2024-04-15 10:38:33 -0600

    Add files via upload

---
+++ EJ1_ZS.cpp
@@ -0,0 +1,29 @@
+# include "iostream"
+# include "string"
+# include "math.h"
+using namespace std;
+int main ()
+{
+	cout <<"Hola este programa 1.8 imprime la matricula y el promedio de 5 notas de un alumno"<< "\n";
+	int matricula, a, b, c, d, e;
+	 cout <<"Introduzca el numero de matricula del alumno: ";
+	 cin>> matricula;
+	 
+	 cout <<"Introduzca la primera calificacion: ";
+	 cin>>a;
+	  cout <<"Introduzca la segunda calificacion: ";
+	 cin>>b;
+	  cout <<"Introduzca la tercera calificacion: ";
+	 cin>>c;
+	  cout <<"Introduzca la cuarta calificacion: ";
+	 cin>>d;
+	  cout <<"Introduzca la quinta calificacion: ";
+	 cin>>e;
+	  cout <<"La matricula del alumno es: " <<matricula<<endl;
+	  
+	 cout <<"El promedio del alumno es: " <<((a+b+c+d+e)/5)<<endl;
+	 cin.get();cin.get();
+	 
+	 return 0;
+	 
+	 }
---
+++ EJ2_ZS.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+
+int  main ()
+ {
+ cout<<"Este programa calcula el cuadrado y el cubo de un numero entero positivo"<< "\n";
+ 
+  int n;
+  cout<<"Ingrese un numero:";
+  cin >> n;
+  
+  cout <<"El numero elevado al cuadrado es: "<<n*n<<endl;
+  
+  cout <<"El numero elevado al cubo es: "<<n*n*n<<endl;
+  
+  return 0;
+  }
